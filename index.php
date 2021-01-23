139aa84e8fd39bf57be591f3a4dcc986a0a2a97d
cyptrid <cyptrid@gmail.com>
2021-01-23 13:54:09 +0700

    Function

--- index.php
+++ index.php
@@ -1,29 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-    <style>
-        .warna-baris{
-            background-color: silver;
-        }
-    </style>
-</head>
-<body>
-    <!-- Control Flow -->
-            <table border="1" cellpadding="10" cellspacing='0'>
-                <?php for ( $i = 1; $i <= 3; $i++) : ?>
-                    <?php if($i % 2 == 1) : ?>
-                    <tr class="warna-baris">
-                    <?php else: ?>
-                    <tr>
-                    <?php endif; ?>
-                        <?php for ( $j = 1; $j <= 5; $j++): ?>
-                            <td><?= "$i, $j"; ?></td>
-                        <?php endfor; ?>
-                    </tr>
-                <?php endfor; ?>
-            </table>
-</body>
-</html>
+<!-- Built-in Function = fungsi yang sudah di sediakan oleh php  -->
+<!-- User-Defined Function = fungsi yang dibuat sendiri -->
+
+<!-- functions
+    Date / Time 
+    time()
+    date()
+    mktime()
+    strtotime()
+
+    String
+    strlen() = menghitung panjang sebuah sting
+    strcmp() = membandingkan 2 buah string
+    exlode() = memecah string menjadi array
+    htmlspecialchars() = mengamankan dari hacker
+
+    utility
+    var_dump() = mencetak isi variable, array, object
+    isset() = mengecek apakah variable sudah di buat / belum
+    empty() = mengecek apakah variable ada isinya / tidak   
+    die() = memberhentikan program
+    sleep() = memberhentikan sementara
+ -->
+
+
+ <!-- User-Defined Function -->
+ <!-- membuat fungsi sendiri -->
+ <?php 
+  function salam($waktu = "Datang", $nama = "Admin"){
+     return "Selamat $waktu, $nama";}
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>PHP</title>
+ </head>
+ <body>
+     <h1><?php echo salam("pagi", "Joban"); ?></h1>
+ </body>
+ </html>
