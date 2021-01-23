a935a69be82477f4c8cb9fb5ee314358a628a8ab
cyptrid <cyptrid@gmail.com>
2021-01-23 13:10:15 +0700

    Control Flow

--- index.php
+++ index.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <style>
+        .warna-baris{
+            background-color: silver;
+        }
+    </style>
 </head>
 <body>
-
+    <!-- Control Flow -->
+            <table border="1" cellpadding="10" cellspacing='0'>
+                <?php for ( $i = 1; $i <= 3; $i++) : ?>
+                    <?php if($i % 2 == 1) : ?>
+                    <tr class="warna-baris">
+                    <?php else: ?>
+                    <tr>
+                    <?php endif; ?>
+                        <?php for ( $j = 1; $j <= 5; $j++): ?>
+                            <td><?= "$i, $j"; ?></td>
+                        <?php endfor; ?>
+                    </tr>
+                <?php endfor; ?>
+            </table>
 </body>
 </html>
