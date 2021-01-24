da52b990dc1a7f91b50901906c938498832dfa7d
cyptrid <cyptrid@gmail.com>
2021-01-24 21:17:18 +0700

    Koneksi Database dengan PHP

--- index.php
+++ index.php
@@ -1,110 +1,46 @@
-<?php
-// Super Global = typenya array associative
-// $_GET
-// $_POST
-// $_REQUEST
-// $_SESSION
-// $_COOKIE
-// $_SERVER
-// $_ENV
-
-// Variable Scope / Lingkup Variable
-// $x = 10;
-// function tampilX(){
-//     global $x;
-//     echo $x;
-// }
-// tampilX();
+<?php 
+    // memanggil function berisi koneksi ke database
+    require 'functions.php';
+   $mahasiswa =  query("SELECT * FROM mahasiswa");
 ?>
 
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-            $mahasiswa = [
-                [  "nama"  =>   "Ikhdanul Widad Joban", 
-                    "nrp"   =>    "082065278",
-                    "email" =>  "jikhdan@gmail.com",
-                    "gambar" => "joban.png"],
-                [   "nama"   =>   " Joban", 
-                    "nrp"   =>    "082065279",
-                    "email" =>  "joban@gmail.com",
-                    "gambar" => "joban1.png",
-                    "tugas" => [90,80,100]
-                ]                    
-            ];
-    ?>
-    <ul>
-        <?php foreach( $mahasiswa as $mhs ): ?>
-            <li>
-                <a href="test.php?name=<?= $mhs["nama"]; ?>"><?= $mhs["nama"]; ?></a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html> -->
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>$_POST</title>
-</head>
-    <body>
-        <form action="test1.php" method="post">
-            Masukkan Nama : <input type="text" name="nama">
-            <button type="submit" name="submit">Kirim</button>
-        </form>
-    </body>
-</html> -->
-
-<!-- Latihan Membuat Sistem Login -->
-<?php 
-                // cek apakah tombol submit sudah ditekan / belum
-                    if( isset($_POST["login"]) ){
-                // cek username dan password
-                    if( $_POST["username"] == "admin" && $_POST["password"] == "123"){
-                // jika benar, redirect ke halaman latihan.php
-                    header("Location: latihan.php");
-                    exit;
-                    }else{
-                // jika salah, tampilkan kesalahan
-                    $error = true;
-                }
-            }
-            ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan</title>
+    <title>Daftar Mahasiswa</title>
 </head>
 <body>
-           
-            <?php if( isset($error) ): ?>
-                <p syle="color:red font-style:italic">Username / Password Salah! </p>
-            <?php endif; ?>
-        <ul>
-            <form action="" method="post">
-                <li>
-                    <label for="username">Username : </label>
-                    <input type="text" name="username" id="username">
-                </li> 
-                <li>
-                    <label for="password">Password : </label>
-                    <input type="password" name="password" id="password">
-                </li> 
-               <li>
-               <button type="submit" name="login">Login</button>
-               </li>
-            </form>
-        </ul>
+        <h1>Daftar Mahasiswa</h1>
+        <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th>No.</th>
+                    <th>Aksi</th>
+                    <th>Gambar</th>
+                    <th>NRP</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>Jurusan</th>
+                </tr>
+            <?php $i = 1; ?>
+        <?php foreach ( $mahasiswa as $row ): ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td>
+                        <a href="">Ubah</a> |
+                        <a href="">Hapus</a>
+                    </td>
+                    <td>
+                        <img src="<?= $row["gambar"]; ?>" width="50" height="50" alt="">
+                    </td>
+                    <td><?= $row["nrp"]; ?></td>
+                    <td><?= $row["nama"]; ?></td>
+                    <td><?= $row["email"]; ?></td>
+                    <td><?= $row["jurusan"]; ?></td>
+                </tr>
+            <?php $i++ ?>
+        <?php endforeach; ?>    
+        </table>
 </body>
 </html>
