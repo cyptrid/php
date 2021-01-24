2e4000a2a113646ae8dbc0aad33470a98d2ea15b
cyptrid <cyptrid@gmail.com>
2021-01-24 22:48:43 +0700

    UPDATE

--- functions.php
+++ functions.php
@@ -35,5 +35,33 @@ function hapus($id){
     mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
     return mysqli_affected_rows($conn);
 }
+
+
+function ubah($data){
+
+    global $conn;
+    //ambil data dari tiap elemen dalam form
+    //mengamankan form dengan htmlspecialchars
+      $id = htmlspecialchars($data["id"]);
+      $nrp = htmlspecialchars($data["nrp"]);
+      $nama = htmlspecialchars($data["nama"]);
+      $email = htmlspecialchars($data["email"]);
+      $jurusan = htmlspecialchars($data["jurusan"]);
+      $gambar = htmlspecialchars($data["gambar"]);
+      // query insert data
+      $query = "UPDATE mahasiswa SET 
+      nrp = '$nrp',
+      nama = '$nama',
+      email = '$email',
+      jurusan = '$jurusan',
+      gambar = '$gambar'
+      WHERE id = $id
+                ";
+       mysqli_query($conn, $query);
+
+     return mysqli_affected_rows($conn);
+}
 ?>
+
+
 
