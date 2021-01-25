a8ea0be19968a972a0aa46b52e348dd3dfb7588a
cyptrid <cyptrid@gmail.com>
2021-01-25 20:18:27 +0700

    COOKIE

--- login.php
+++ login.php
@@ -1,12 +1,27 @@
 <?php 
 session_start();
+require 'functions.php';
+
+    // set cookie
+    if( isset($_COOKIE['id']) && isset($_COOKIE['key']) ){
+        $id = $_COOKIE['id'];
+        $key = $_COOKIE['key'];
+
+        // ambil username berdasarkan id
+        $result = mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+        $row = mysqli_fetch_assoc($result);
+        
+        //cek cooke dan username
+        if( $key === hash('sha256', $row['username']) ){
+            $_SESSION['login'] = true;
+        }
+    }
 
     if( isset($_SESSION["login"]) ){
         header("Location: index.php");
         exit;
     }
     
-require 'functions.php';
 
     if( isset($_POST["login"]) ){
         
@@ -22,6 +37,13 @@ require 'functions.php';
                 if(  password_verify($password, $row["password"]) ){
                     // set session
                     $_SESSION["login"] = true;
+
+                    // cek remember me
+                    if( isset($_POST['remember']) ){
+                        // buat cookie
+                        setcookie('id', $row['id'], time()+60);
+                        setcookie('key', hash('sha256', $row['username']), time() + 60 );
+                    }
 
                     header("Location: index.php");
                     exit;
@@ -55,6 +77,10 @@ require 'functions.php';
             <li>
                 <label for="password">Password : </label>
                 <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember Me</label>
             </li>
             <li>
                 <button type="submit" name="login">Login</button>
